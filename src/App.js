c5085c46ebcc5dfca915a382175d03f2d6957144
root <testhortela1n>
2020-04-11 14:25:08 +0000

    Final version

--- src/App.js
+++ src/App.js
@@ -1,114 +1,109 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
-import DisplayNames from './DisplayNames'
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import DisplayNames from "./DisplayNames"
 
-/*
-Use React and the data below to display a list of users alongside their favorite movies.
-
-For detailed instructions, refer to instructions.md.
-*/
 
 const profiles = [
   {
     id: 1,
-    userID: '1',
-    favoriteMovieID: '1',
+    userID: "1",
+    favoriteMovieID: "1"
   },
   {
     id: 2,
-    userID: '2',
-    favoriteMovieID: '1',
+    userID: "2",
+    favoriteMovieID: "1"
   },
   {
     id: 3,
-    userID: '4',
-    favoriteMovieID: '5',
+    userID: "4",
+    favoriteMovieID: "5"
   },
   {
     id: 4,
-    userID: '5',
-    favoriteMovieID: '2',
+    userID: "5",
+    favoriteMovieID: "2"
   },
   {
     id: 5,
-    userID: '3',
-    favoriteMovieID: '5',
+    userID: "3",
+    favoriteMovieID: "5"
   },
   {
     id: 6,
-    userID: '6',
-    favoriteMovieID: '4',
-  },
+    userID: "6",
+    favoriteMovieID: "4"
+  }
 ];
 
 const users = {
   1: {
     id: 1,
-    name: 'Jane Cruz',
-    userName: 'coder',
+    name: "Jane Cruz",
+    userName: "coder"
   },
   2: {
     id: 2,
-    name: 'Matthew Johnson',
-    userName: 'mpage',
+    name: "Matthew Johnson",
+    userName: "mpage"
   },
   3: {
     id: 3,
-    name: 'Autumn Green',
-    userName: 'user123',
+    name: "Autumn Green",
+    userName: "user123"
   },
   4: {
-    id: 4,
-    name: 'John Doe',
-    userName: 'user123',
+    id: 3,
+    name: "John Doe",
+    userName: "user123"
   },
   5: {
     id: 5,
-    name: 'Lauren Carlson',
-    userName: 'user123',
+    name: "Lauren Carlson",
+    userName: "user123"
   },
   6: {
     id: 6,
-    name: 'Nicholas Lain',
-    userName: 'user123',
-  },
+    name: "Nicholas Lain",
+    userName: "user123"
+  }
 };
 
 const movies = {
   1: {
     id: 1,
-    name: 'Planet Earth 1',
+    name: "Planet Earth"
   },
   2: {
     id: 2,
-    name: 'Selma',
+    name: "Selma"
   },
   3: {
     id: 3,
-    name: 'Million Dollar Baby',
+    name: "Million Dollar Baby"
   },
   4: {
     id: 4,
-    name: 'Forrest Gump',
+    name: "Forrest Gump"
   },
   5: {
     id: 5,
-    name: 'Get Out',
-  },
+    name: "Get Out"
+  }
 };
 
 class App extends Component {
   render() {
+    /*Return JSX*/
     return (
-      <div>
+        <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <h2>Favorite Movies</h2>
-		<DisplayNames users={users} profiles={profiles} movies={movies} />
-      </div>
+      <DisplayNames profiles={profiles} movies={movies} users={users}/>
+	  </div>
     );
   }
 }
